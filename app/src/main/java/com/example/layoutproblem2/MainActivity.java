1a738af4765e91fb555bf095e6818b4243d7e794
Andriy <ak8022@bs.amalnet.k12.il>
2020-03-16 22:17:33 +0200

    Design

--- app/src/main/java/com/example/layoutproblem2/MainActivity.java
+++ app/src/main/java/com/example/layoutproblem2/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.layoutproblem2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    int counter;
+    TextView counterTV;
+    TextView trueFalseTV;
+    TextView num1TV;
+    TextView num2TV;
+    int num1;
+    int num2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counter=0;
+        counterTV=(TextView)findViewById(R.id.textView);
+        trueFalseTV=(TextView)findViewById(R.id.textView2);
+        num1TV=(TextView)findViewById(R.id.textView4);
+        num2TV=(TextView)findViewById(R.id.textView5);
+    }
+
+    public void reset(View view)
+    {
+        counter=0;
+        counterTV.setText("Counter: "+counter);
     }
 }
